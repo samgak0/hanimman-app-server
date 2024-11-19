2e7ed167e8b5f2b5b567a93fc531ee8d1c561915
samgak <183370871+samgak0@users.noreply.github.com>
2024-11-19 11:53:57 +0900

    고침: contextLoads 테스트 활성화

--- src/test/java/org/devkirby/app/hanimman/HanimmanApplicationTests.java
+++ src/test/java/org/devkirby/app/hanimman/HanimmanApplicationTests.java
@@ -1,13 +1,11 @@
 package org.devkirby.app.hanimman;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HanimmanApplicationTests {
-	/*
-	 * @Test
-	 * void contextLoads() {
-	 * }
-	 */
-
+	 @Test
+	 void contextLoads() {
+	 }
 }
